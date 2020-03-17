4b0ebc70696294a5b9acecf82f9a2963d48f84d0
ImFireGod <49344172+ImFireGod@users.noreply.github.com>
2020-03-17 22:55:05 +0100

    Add files via upload

--- src/fr/imfiregod/murder/Main.java
+++ src/fr/imfiregod/murder/Main.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.imfiregod.utils.FastBoard;
@@ -12,6 +13,11 @@ public class Main extends JavaPlugin {
 
     private final Map<UUID, FastBoard> boards = new HashMap<>();
     private GameState state;
+    
+    @Override
+    public void onEnable() {
+		Bukkit.getConsoleSender().sendMessage("§6Murder §e| Chargement du plugin cree par §6TheStorm§e et §6ImFireGod§e.");	
+    }
     
     public GameState getState() {
     	return this.state;
