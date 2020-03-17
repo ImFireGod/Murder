42e04a187abcc12383ca10a7c8c9627690817e0f
ImFireGod <49344172+ImFireGod@users.noreply.github.com>
2020-03-17 22:50:21 +0100

    Update Main.java

--- src/fr/imfiregod/murder/Main.java
+++ src/fr/imfiregod/murder/Main.java
@@ -3,5 +3,7 @@ package fr.imfiregod.murder;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+  
+  private final Map<UUID, FastBoard> boards = new HashMap<>();
 
 }
