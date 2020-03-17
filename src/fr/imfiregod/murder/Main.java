f22c282f975a349e4e7eb428f54ddb00c7cc17d2
ImFireGod <49344172+ImFireGod@users.noreply.github.com>
2020-03-17 22:53:24 +0100

    Add files via upload

--- src/fr/imfiregod/murder/Main.java
+++ src/fr/imfiregod/murder/Main.java
@@ -1,9 +1,23 @@
 package fr.imfiregod.murder;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin {
-  
-  private final Map<UUID, FastBoard> boards = new HashMap<>();
+import fr.imfiregod.utils.FastBoard;
 
+public class Main extends JavaPlugin {
+
+    private final Map<UUID, FastBoard> boards = new HashMap<>();
+    private GameState state;
+    
+    public GameState getState() {
+    	return this.state;
+    }
+    
+    public void setState(GameState statut) {
+    	this.state = statut;
+    }
 }
